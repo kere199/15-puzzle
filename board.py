8feae3c6d530b139615a0b276c33d3219d8bf4f8
kere199 <kereleishviliirakli81@gmail.com>
2023-10-18 09:24:18 +0400

    swap with void

--- board.py
+++ board.py
@@ -7,9 +7,7 @@ class Board:
      self.t.goto(-100,100)
      self.t.setheading(0)
      self.tiles = [[1,2,3,4], [5,6,7,8],[9,10,11,12], [13,14,15,None]]
-     self.y = 100
-
-
+     self.voidpos = (3,3)
           
 
     def draw_tiles(self):
@@ -18,9 +16,18 @@ class Board:
              self.t.write(num)
              self.t.forward(50) 
           self.t.goto(-100, self.t.ycor() - 50)
-        
-screen = Screen()     
 
+
+    def swap_with_void(self,pos):
+       x1, y1 = pos
+       x0, y0 = self.voidpos
+       self.tiles[x0][y0],self.tiles[x1][y1] = self.tiles[x1][y1], self.tiles[x0][y0]
+
+    def down(self):
+       el_up_pos = (self.voidpos [0] , self.voidpos[1]) 
+       self.swap_with_void(el_up_pos)
+               
+screen = Screen()
 
 board = Board()
 board.draw_tiles()
