e1778bcba59f8e94e97fe50aa698c74f23f5edbb
kere199 <kereleishviliirakli81@gmail.com>
2023-10-18 21:46:41 +0400

    add moving of the void to each direction

--- board.py
+++ board.py
@@ -4,11 +4,17 @@ class Board:
      self.t = Turtle()
      self.t.hideturtle()
      self.t.penup()
-     self.t.goto(-100,100)
+     self.t.goto(-100,300)
      self.t.setheading(0)
      self.tiles = [[1,2,3,4], [5,6,7,8],[9,10,11,12], [13,14,15,None]]
-     self.voidpos = (3,3)
+    #  self.voidpos = self.find_void_position()
           
+
+    def find_void_position(self):
+        for i in range(len(self.tiles)):
+            for j in range(len(self.tiles[i])):
+                if self.tiles[i][j] == None:
+                    return (i, j)
 
     def draw_tiles(self):
        for row  in self.tiles:
@@ -20,14 +26,58 @@ class Board:
 
     def swap_with_void(self,pos):
        x1, y1 = pos
-       x0, y0 = self.voidpos
+       x0, y0 = self.find_void_position()
        self.tiles[x0][y0],self.tiles[x1][y1] = self.tiles[x1][y1], self.tiles[x0][y0]
 
     def down(self):
-       el_up_pos = (self.voidpos [0] , self.voidpos[1]) 
-       self.swap_with_void(el_up_pos)
-               
-screen = Screen()
+       el_up_pos = (self.find_void_position() [0] -1 , self.find_void_position()[1]) 
+       if 0 <= el_up_pos[0] <= 3 and 0 <= el_up_pos[1] <= 3:
+        self.swap_with_void(el_up_pos)
+        self.t.goto(-100,300)
+        self.t.clear()
+        self.draw_tiles()
 
+    def up(self):
+       el_down_pos = (self.find_void_position()[0] +1  , self.find_void_position()[1]) 
+       if 0 <= el_down_pos[0] <= 3 and 0 <= el_down_pos[1] <= 3:
+        self.swap_with_void(el_down_pos)
+        self.t.goto(-100,300)
+        self.t.clear()
+        self.draw_tiles()
+
+
+    def left(self):
+       el_right_pos = (self.find_void_position()[0] , self.find_void_position()[1]-1) 
+       if 0 <= el_right_pos[0] <= 3 and 0 <= el_right_pos[1] <= 3:
+        self.swap_with_void(el_right_pos)
+        self.t.goto(-100,300)
+        self.t.clear()
+        self.draw_tiles()
+
+
+    def right(self):
+       el_left_pos = (self.find_void_position()[0] , self.find_void_position()[1]+1) 
+       if 0 <= el_left_pos[0] <= 3 and 0 <= el_left_pos[1] <= 3:
+        self.swap_with_void(el_left_pos)
+        self.t.goto(-100,300)
+        self.t.clear()
+        self.draw_tiles()
+
+
+
+
+               
 board = Board()
 board.draw_tiles()
+screen = Screen()
+screen.tracer(0)
+screen.listen()
+screen.onkeypress(fun= board.down, key= "Down" )
+screen.onkeypress(fun= board.up, key= "Up" )
+screen.onkeypress(fun= board.left, key= "Left" )
+screen.onkeypress(fun= board.right, key= "Right" )
+
+
+
+
+screen.exitonclick()
